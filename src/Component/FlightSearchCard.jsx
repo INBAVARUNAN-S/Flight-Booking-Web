11b8d7c2503840965d36b40e44edd8fd51470f38
INBAVARUNAN-S <inbavarunans.bcy24@rathinam.in>
2025-08-28 00:11:41 +0530

    updated

--- src/Component/FlightSearchCard.jsx
+++ src/Component/FlightSearchCard.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const FlightSearchCard = () => {
 
@@ -10,25 +12,31 @@ const FlightSearchCard = () => {
                 <Card.Body>
                     <div className="row">
                         <div className='col-md-6 col-lg-6 col-xl-6 col-sm-12 fs-6'>
-                            <Card.Title className=''>Book a Flight</Card.Title>
-                            <Card.Subtitle className="mb-2 text-muted">Where would you want to go ?</Card.Subtitle>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                            </Card.Text>
-                            <Card.Link href="#">Card Link</Card.Link>
-                            <Card.Link href="#">Another Link</Card.Link>
+                            <Card.Subtitle className="mb-2 ">Where would you want to go ?</Card.Subtitle>
+                            {/* <Card.Title className='fs-1'>Book a Flight</Card.Title>    */}
+                            <img src={"https://5.imimg.com/data5/CA/HH/FC/SELLER-102861858/flight-booking-500x500.jpg"} className='flight-img' alt="" />
                         </div>
-
                         <div className='col-md-6 col-lg-6 col-xl-6 col-sm-12'>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                            </Card.Text>
-                            <Card.Link href="#">Card Link</Card.Link>
-                            <Card.Link href="#">Another Link</Card.Link>
+                            <Form>
+                                <Form.Group className="mb-2" controlId="formBasicEmail">
+                                    <Form.Label>From</Form.Label>
+                                    <Form.Control type="text" placeholder="Coimbatore" />
+                                </Form.Group>
+
+                                <Form.Group className="mb-2" controlId="formBasicPassword">
+                                    <Form.Label>To</Form.Label>
+                                    <Form.Control type="text" placeholder="Delhi" />
+                                </Form.Group>
+
+                                <Form.Group className="mb-2" controlId="formBasicPassword">
+                                    <Form.Label>Date</Form.Label>
+                                    <Form.Control type="date" />
+                                </Form.Group> 
+
+                                <Button variant="success" className='w-100 btn-color my-4 py-3' type="submit">
+                                    Search
+                                </Button>  
+                            </Form>
                         </div>
                     </div>
 
