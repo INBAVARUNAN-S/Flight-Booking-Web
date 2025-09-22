b79b1f1e81f1c8d15f72800b211a309602da699d
INBAVARUNAN-S <inbavarunans.bcy24@rathinam.in>
2025-09-22 15:35:02 +0530

    updated

--- Client/src/Component/Main.jsx
+++ Client/src/Component/Main.jsx
@@ -8,12 +8,25 @@ const Main = () => {
 
   const [flights, setFlights] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [singleData, setSingleData] = useState(); 
 
   const getApiData = async (from, to, date) => {
-    const response = await axios(`http://localhost:3000/api/flights?from=${from}&to=${to}&date=${date}`);
+    try{
+      const response = await axios(`http://localhost:3000/api/flights?from=${from}&to=${to}&date=${date}`);
+  
+      if (response.data || response.data.length > 0) {
+        setFlights(response.data);   
+        setSingleData(response.data[0]); 
+        setLoading(false);
+      } else {
+        setLoading(true)
+      }
 
-    setFlights(response.data);
-    setLoading(false);
+    }catch(err){
+      console.log(err);
+    }finally{
+      setLoading(false);
+    }
   }
 
   // if (loading) {
@@ -27,28 +40,37 @@ const Main = () => {
 
         <FlightSearchCard getData={getApiData} />
         <div className='my-3'>
-          <div className='d-flex align-items-center justify-content-between gap-3'>
-            <div>
-              <p className='mb-1'>From</p>
-              <h4 className='mb-1'>Coimbatore</h4>
-              <p className='mb-1'>CCA</p>
-            </div>
-            <hr className='flex-fill' style={{ borderStyle: 'dashed' }} />
-            <div className='text-end'>
-              <p className='mb-1'>To</p>
-              <h4 className='mb-1'>NYC</h4>
-              <p className='mb-1'>NYC Airport</p>
-            </div>
-          </div>
-        </div>
-        {
-          loading ? <h1>Loading...</h1> : flights.map((flight) => {
-            return ( 
-              <FlightDetailCard flight={flight} />
-            )
-          })
-        }
+          {
+            loading ? <h1>Loading...</h1> : <>
 
+              <div className='d-flex align-items-center justify-content-between gap-3'>
+
+                <div>
+                  <p className='mb-1'>From</p>
+                  <h4 className='mb-1'>{singleData.departure.city}</h4>
+                  <p className='mb-1'>{singleData.departure.airport}</p>
+                </div>
+                <hr className='flex-fill' style={{ borderStyle: 'dashed' }} /> 
+                <div className='text-end'>
+                  <p className='mb-1'>To</p>
+                  <h4 className='mb-1'>{singleData.arrival.city}</h4>
+                  <p className='mb-1'>{singleData.arrival.airport}</p>
+                </div>
+
+              </div>
+            </>
+          }
+        </div>
+
+
+
+        {/* {
+              loading ? <h1>Loading...</h1> : flights.map((flight) => {
+                return (
+                  <FlightDetailCard flight={flight} />
+                )
+              })
+            } */}
 
         <div>
 
